9bac9a5de4f15e22137df98bb6e870243640d395
Zormz <115802948+Zormzy@users.noreply.github.com>
2023-11-10 11:35:47 +0100

    Gamepad movement fix with joystick vector. Creation combat test scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,12 +7,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerMoveManager _playerMoveManager;
     [SerializeField] private PlayerJumpManager _playerJumpManager;
 
+    [Header("Controller values")]
+    private string _controllerName;
     private Vector2 movementInput;
+    private Vector2 _contextInput;
+
+    private void Awake()
+    {
+        _controllerName = null;
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        if (movementInput != context.ReadValue<Vector2>())
-            movementInput = context.ReadValue<Vector2>();
+        if (_controllerName == null)
+            _controllerName = context.control.device.displayName;
+
+        if (_controllerName == "Keyboard")
+        {
+            if (movementInput != context.ReadValue<Vector2>())
+                movementInput = context.ReadValue<Vector2>();
+        }
+        else
+        {
+            _contextInput.Set(context.ReadValue<Vector2>().x, 0);
+
+            if (movementInput != _contextInput)
+                movementInput = _contextInput;
+        }
 
         _playerMoveManager._playerMovementDirection = movementInput;
         _playerMoveManager._isMoving = context.performed;
@@ -20,12 +41,18 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        if (_controllerName == null)
+            _controllerName = context.control.device.displayName;
+
         _playerJumpManager._playerDirection = movementInput;
         _playerJumpManager.PlayerJumpBuffer(context.ReadValueAsButton());
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
+        if (_controllerName == null)
+            _controllerName = context.control.device.displayName;
+
 
     }
 }
