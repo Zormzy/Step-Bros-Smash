5767e7e8c274d3f796fa78b1f2cca1260973509c
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2023-11-14 10:52:09 +0100

    UI
    Activate each player's UI when they join the game, view each player's damage percentages, display their name and team

--- Assets/Scripts/Player/PlayerInfos.cs
+++ Assets/Scripts/Player/PlayerInfos.cs
@@ -8,12 +8,11 @@ public class PlayerInfos : MonoBehaviour
     public Vector3 startPos;
     public int life;
     public float damagesPercent;
+    public int team;
 
     void Start()
     {
         transform.position = startPos;
         life = 3;
     }
-
-    
 }
--- Assets/Scripts/Player/PlayerSpawnManager.cs
+++ Assets/Scripts/Player/PlayerSpawnManager.cs
@@ -6,10 +6,13 @@ using UnityEngine.InputSystem;
 public class PlayerSpawnManager : MonoBehaviour
 {
     public Transform[] spawnPositions;
+    public UiManager uiManager;
 
     void OnPlayerJoined(PlayerInput playerInput)
     {
         playerInput.gameObject.GetComponent<PlayerInfos>().playerID = playerInput.playerIndex + 1;
+        playerInput.gameObject.GetComponent<PlayerInfos>().team = playerInput.playerIndex + 1; //a suppr en créant les menus
         playerInput.gameObject.GetComponent<PlayerInfos>().startPos = spawnPositions[playerInput.playerIndex].position;
+        uiManager.newPlayerJoined = true;
     }
 }
