12d99835cd1f98fd17770e375599fa0a6391db8e
Zormz <115802948+Zormzy@users.noreply.github.com>
2023-11-14 16:00:41 +0100

    Player stun while being hit. Adding death zone all axes.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,18 +8,30 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerJumpManager _playerJumpManager;
     [SerializeField] private PlayerAttackManager _playerAttackManager;
     [SerializeField] private PlayerParryManager _playerParryManager;
+    [SerializeField] private PlayerInput _playerInput;
 
     [Header("Controller values")]
     private Vector2 movementInput;
     private Vector2 _contextInput;
     private Vector2 _attackMovementInput;
     private bool _downMovementBtn;
+    private bool _isStuned;
+    private float _stunTimer;
+    private float _stunTimerCounter;
 
     private void Awake()
     {
         _downMovementBtn = false;
+        _isStuned = false;
         movementInput = Vector2.zero;
         _attackMovementInput = movementInput;
+        _stunTimer = 0.2f;
+        _stunTimerCounter = 0f;
+    }
+
+    private void Update()
+    {
+        OnPlayerStun();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -61,5 +73,25 @@ public class PlayerController : MonoBehaviour
     public void OnParry(InputAction.CallbackContext context)
     {
         _playerParryManager.PlayerParry(context.ReadValueAsButton());
+    }
+
+    private void OnPlayerStun()
+    {
+        if (_isStuned)
+            _stunTimerCounter -= Time.deltaTime;
+        else if (_stunTimerCounter != _stunTimer)
+            _stunTimerCounter = _stunTimer;
+
+        if (_isStuned && _stunTimerCounter <= 0)
+        {
+            _playerInput.ActivateInput();
+            _isStuned = false;
+        }
+    }
+
+    public void OnPlayerStunAction()
+    {
+        _playerInput.DeactivateInput();
+        _isStuned = true;
     }
 }
