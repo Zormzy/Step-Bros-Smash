ebc0ed46b798694cab9711ee082c9e640331e3f9
Zormz <115802948+Zormzy@users.noreply.github.com>
2023-11-17 18:01:30 +0100

    Revert "Add menu to develop"

--- Assets/Scripts/Player/PlayerInfos.cs
+++ Assets/Scripts/Player/PlayerInfos.cs
@@ -8,45 +8,10 @@ public class PlayerInfos : MonoBehaviour
     public float damagesPercent;
     public int team;
 
-    public int chosenCharacter;
-    public GameObject characterPrefab1;
-    public GameObject characterPrefab2;
-
     void Start()
     {
-        Debug.Log(playerID);
-        DontDestroyOnLoad(gameObject);
-        LoadPlayerInput();
-
+        transform.position = startPos;
         life = 3;
-    }
-
-    private void Update()
-    {
-        Debug.Log(playerID);
-
-    }
-
-    public void SetPrefab()
-    {
-        if(chosenCharacter == 1)
-        {
-
-        }
-        else if(chosenCharacter == 2)
-        {
-
-        }
-    }
-    public void SavePlayerInput()
-    {        
-        PlayerPrefs.SetInt("PlayerID", playerID);
-        PlayerPrefs.Save();
-    }
-
-    public void LoadPlayerInput()
-    {
-        playerID = PlayerPrefs.GetInt("PlayerID");
     }
 
     public void ResetDamages()
