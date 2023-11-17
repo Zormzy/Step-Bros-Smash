a2d72510722cdbd2799b9df43274f2c47a744077
Zormz <115802948+Zormzy@users.noreply.github.com>
2023-11-17 16:31:39 +0100

    Player SFX fix with multiple audiosource.

--- Assets/Scripts/Player/PlayerAudioManager.cs
+++ Assets/Scripts/Player/PlayerAudioManager.cs
@@ -3,29 +3,30 @@ using UnityEngine;
 public class PlayerAudioManager : MonoBehaviour
 {
     [Header("Attack SFX")]
-    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSourceAttack;
+    [SerializeField] private AudioSource _audioSourceJump;
     [SerializeField] private AudioClip _attackSFX;
     [SerializeField] private AudioClip _jumpSFX;
     [SerializeField] private AudioClip _doubleJumpSFX;
 
     public void AudioSFXOnAttack()
     {
-        _audioSource.clip = _attackSFX;
-        _audioSource.volume = 1f;
-        _audioSource.Play();
+        _audioSourceJump.clip = _attackSFX;
+        _audioSourceJump.volume = 1f;
+        _audioSourceJump.Play();
     }
 
     public void AudioSFXOnJump()
     {
-        _audioSource.clip = _jumpSFX;
-        _audioSource.volume = 1f;
-        _audioSource.Play();
+        _audioSourceJump.clip = _jumpSFX;
+        _audioSourceJump.volume = 1f;
+        _audioSourceJump.Play();
     }
 
     public void AudioSFXOnDoubleJump()
     {
-        _audioSource.clip = _doubleJumpSFX;
-        _audioSource.volume = 1f;
-        _audioSource.Play();
+        _audioSourceJump.clip = _doubleJumpSFX;
+        _audioSourceJump.volume = 1f;
+        _audioSourceJump.Play();
     }
 }
